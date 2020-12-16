deeeb57327ba92a1708ed169f8921d8c96287b64
JunkiYoshida <junkiyoshida@JunkiYoshidanoMacBook-Air.local>
2020-12-16 14:34:02 +0900

    feat: dynamodb

--- serverless.ts
+++ serverless.ts
@@ -11,9 +11,92 @@ const serverlessConfiguration: AWS = {
     'serverless-offline': {
       httpPort: 8080,
     },
+    dynamodb: {
+      stages: ['dev'],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+        seed: false,
+        sharedDB: true,
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      JankensTable: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: 'jankens',
+          AttributeDefinitions: [
+            {
+              AttributeName: 'player',
+              AttributeType: 'S',
+            },
+            {
+              AttributeName: 'unixtime',
+              AttributeType: 'N',
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'player',
+              KeyType: 'HASH',
+            },
+            {
+              AttributeName: 'unixtime',
+              KeyType: 'RANGE',
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+        },
+      },
+      OptionsTables: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: 'options',
+          AttributeDefinitions: [
+            {
+              AttributeName: 'uuid',
+              AttributeType: 'S',
+            },
+            {
+              AttributeName: 'title',
+              AttributeType: 'S',
+            },
+            // テーブル作成時は不要
+            // {
+            //   AttributeName: 'createdAt',
+            //   AttributeType: 'S',
+            // },
+            // {
+            //   AttributeName: 'updatedAt',
+            //   AttributeType: 'S',
+            // },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'uuid',
+              KeyType: 'HASH',
+            },
+            {
+              AttributeName: 'title',
+              KeyType: 'RANGE',
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+        },
+      },
+    },
   },
   // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack', 'serverless-offline'],
+  plugins: ['serverless-webpack', 'serverless-offline', 'serverless-dynamodb-local'],
   provider: {
     name: 'aws',
     runtime: 'nodejs12.x',
