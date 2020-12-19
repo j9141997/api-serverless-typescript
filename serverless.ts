36afa141b86f83bc61553e6429f68786a088f44f
JunkiYoshida <junkiyoshida@JunkiYoshidanoMacBook-Air.local>
2020-12-19 21:42:44 +0900

    feat: seeting dynamodb offline

--- serverless.ts
+++ serverless.ts
@@ -1,4 +1,5 @@
 import type { AWS } from '@serverless/typescript';
+import options from './dynamodb/migrations/options';
 
 const serverlessConfiguration: AWS = {
   service: 'my-api',
@@ -17,85 +18,26 @@ const serverlessConfiguration: AWS = {
         port: 8000,
         inMemory: true,
         migrate: true,
-        seed: false,
+        seed: true,
         sharedDB: true,
+      },
+      seed: {
+        dev: {
+          sources: [
+            {
+              table: 'options',
+              sources: ['./dynamodb/seeds/options.json'],
+            },
+          ],
+        },
       },
     },
   },
   resources: {
     Resources: {
-      JankensTable: {
-        Type: 'AWS::DynamoDB::Table',
-        Properties: {
-          TableName: 'jankens',
-          AttributeDefinitions: [
-            {
-              AttributeName: 'player',
-              AttributeType: 'S',
-            },
-            {
-              AttributeName: 'unixtime',
-              AttributeType: 'N',
-            },
-          ],
-          KeySchema: [
-            {
-              AttributeName: 'player',
-              KeyType: 'HASH',
-            },
-            {
-              AttributeName: 'unixtime',
-              KeyType: 'RANGE',
-            },
-          ],
-          ProvisionedThroughput: {
-            ReadCapacityUnits: 1,
-            WriteCapacityUnits: 1,
-          },
-        },
-      },
-      OptionsTables: {
-        Type: 'AWS::DynamoDB::Table',
-        Properties: {
-          TableName: 'options',
-          AttributeDefinitions: [
-            {
-              AttributeName: 'uuid',
-              AttributeType: 'S',
-            },
-            {
-              AttributeName: 'title',
-              AttributeType: 'S',
-            },
-            // テーブル作成時は不要
-            // {
-            //   AttributeName: 'createdAt',
-            //   AttributeType: 'S',
-            // },
-            // {
-            //   AttributeName: 'updatedAt',
-            //   AttributeType: 'S',
-            // },
-          ],
-          KeySchema: [
-            {
-              AttributeName: 'uuid',
-              KeyType: 'HASH',
-            },
-            {
-              AttributeName: 'title',
-              KeyType: 'RANGE',
-            },
-          ],
-          ProvisionedThroughput: {
-            ReadCapacityUnits: 1,
-            WriteCapacityUnits: 1,
-          },
-        },
-      },
+      optionsTables: options,
     },
   },
-  // Add the serverless-webpack plugin
   plugins: ['serverless-webpack', 'serverless-offline', 'serverless-dynamodb-local'],
   provider: {
     name: 'aws',
